b17571e0a49c14c219bfbc024f41ab7a4576b5cf
shika-hachi <shika_hachi24@outlook.jp>
2017-06-16 20:49:41 +0900

    メインページ作成中

---
+++ CopipeDictionary/View/Command.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CopipeDictionary.View
+{
+    public class Command : ICommand
+    {
+        /// <summary>
+        /// コマンドを実行するかどうかに影響するような変更があった場合に発生します。
+        /// </summary>
+        public event EventHandler CanExecuteChanged;
+
+        /// <summary>
+        /// コマンド実行時に呼び出すアクション
+        /// </summary>
+        private readonly Action<object> _commandExecuted;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="commandExecuted">コマンド実行時に呼び出すアクション</param>
+        public Command(Action commandExecuted)
+        {
+            this._commandExecuted = (parameter => commandExecuted());
+        }
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="commandExecuted">コマンド実行時に呼び出すアクション</param>
+        public Command(Action<object> commandExecuted)
+        {
+            this._commandExecuted = commandExecuted;
+        }
+
+        /// <summary>
+        /// 現在の状態でコマンドが実行可能かどうかを決定するメソッドを定義します。
+        /// </summary>
+        /// <param name="parameter">コマンドにより使用されるデータです。コマンドにデータを渡す必要がない場合は、このオブジェクトを null に設定できます。</param>
+        /// <returns>このコマンドを実行できる場合は true。それ以外の場合は false。</returns>
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// コマンドが起動される際に呼び出すメソッドを定義します。
+        /// </summary>
+        /// <param name="parameter">コマンドにより使用されるデータです。コマンドにデータを渡す必要がない場合は、このオブジェクトを null に設定できます。</param>
+        public void Execute(object parameter)
+        {
+            this._commandExecuted(parameter);
+        }
+    }
+}
---
+++ CopipeDictionary/View/MainPageViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+using Windows.ApplicationModel.DataTransfer;
+
+namespace CopipeDictionary.View
+{
+    /// <summary>
+    /// メインページのビューモデル
+    /// </summary>
+    public class MainPageViewModel
+    {
+        /// <summary>
+        /// リストに表示するテキストの一覧
+        /// </summary>
+        public ObservableCollection<StringItem> StringList { get; private set; }
+        /// <summary>
+        /// リストのボタン押下時のコマンド
+        /// </summary>
+        public Command ListItemButtonCommand { get; private set; }
+        /// <summary>
+        /// アプリバー追加ボタン押下時のコマンド
+        /// </summary>
+        public Command AppBarAddButtonCommand { get; private set; }
+
+        /// <summary>
+        /// コンストラクタ。
+        /// </summary>
+        public MainPageViewModel()
+        {
+            this.StringList = new ObservableCollection<StringItem>();
+
+            this.ListItemButtonCommand = new Command(this.ListItemButton_Clicked);
+            this.AppBarAddButtonCommand = new Command(this.AppBarAddButton_Clicked);
+        }
+
+        public void ListItemButton_Clicked(object item)
+        {
+            StringItem si = item as StringItem;
+            if (null != si)
+            {
+                DataPackage dataPackage = new DataPackage();
+                dataPackage.RequestedOperation = DataPackageOperation.Copy;
+                dataPackage.SetText(si.Text);
+                Clipboard.SetContent(dataPackage);
+            }
+        }
+
+        /// <summary>
+        /// アプリバー追加ボタン押下時に呼ばれる。
+        /// </summary>
+        public void AppBarAddButton_Clicked()
+        {
+            this.StringList.Add(new StringItem() { Text = "aaa" });
+        }
+    }
+
+    public class StringItem
+    {
+        public string Text { get; set; }
+    }
+}
